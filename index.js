085eccd7aaa71ffa424c37c22ced587556a04185
Pablo Ariel Bilevich Alaniz <pablobvch@hotmail.com>
2019-11-13 17:15:46 -0300

    feat: set port 5000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pdfTemplate = require("./documents");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
